df3fe7929de3eabb295066e76da4e05bb54eae31
lolkekw1 <kim635107@gmail.com>
2024-03-28 16:01:24 +0500

    android project

---
+++ Android/app/src/main/java/com/app/tf2wiki/MainActivity.java
@@ -0,0 +1,27 @@
+package com.app.tf2wiki;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton imageButton = findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Интент для перехода на главный экран
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
